a498cb9a50e76b7264efb4950f889ea9d1eb26a5
Riccardo Torello <rikitorello2001@gmail.com>
2020-03-16 19:20:37 +0100

    Realizzazione di un progetto xml

--- Demoxml/Demoxml/MainWindow.xaml.cs
+++ Demoxml/Demoxml/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace Demoxml
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		List<Calciatore> calciatori;
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -28,13 +29,31 @@ namespace Demoxml
 
 		private void Btn_aggiorna_Click(object sender, RoutedEventArgs e)
 		{
+			Task.Factory.StartNew(()=>CaricaDati());
+
+		}
+
+		private void CaricaDati()
+		{
+			calciatori = new List<Calciatore>();
 			string path = @"Calciatori.xml";
 			XDocument xmlDoc = XDocument.Load(path);
 			XElement xmlcalciatori = xmlDoc.Element("calciatori");
 			var xmlcalciatore = xmlcalciatori.Elements("calciatore");
-
-
-
+			foreach (var item in xmlcalciatore)
+			{
+				XElement xmlFirstName = item.Element("nome");
+				XElement xmlLastName = item.Element("cognome");
+				XElement xmlSquadra = item.Element("squadra");
+				XElement xmlNascita = item.Element("data");
+				Calciatore a = new Calciatore();
+				a.Nome = xmlFirstName.Value;
+				a.Cognome = xmlLastName.Value;
+				a.Squadra = xmlSquadra.Value;
+				a.Data = Convert.ToDateTime(xmlNascita.Value);
+				calciatori.Add(a);
+			}
+			Dispatcher.Invoke(()=>Lst_calciatori.ItemsSource = calciatori);
 		}
 	}
 }
