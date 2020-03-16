18eb8980a188664d9a7e13e4ceb3ce2b7ea121bd
Riccardo Torello <rikitorello2001@gmail.com>
2020-03-16 18:57:15 +0100

    Creazione di un progetto xml

---
+++ Demoxml/Demoxml/Calciatore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demoxml
+{
+	public class Calciatore
+	{
+		public string Nome { get; set; }
+		public string Cognome { get; set; }
+		public string Squadra { get; set; }
+		public DateTime Data { get; set; }
+
+		public override string ToString()
+		{
+			return $"{Nome} {Cognome}";
+		}
+	}
+}
---
+++ Demoxml/Demoxml/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Demoxml
+{
+	/// <summary>
+	/// Logica di interazione per MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Btn_aggiorna_Click(object sender, RoutedEventArgs e)
+		{
+			string path = @"Calciatori.xml";
+			XDocument xmlDoc = XDocument.Load(path);
+			XElement xmlcalciatori = xmlDoc.Element("calciatori");
+			var xmlcalciatore = xmlcalciatori.Elements("calciatore");
+
+
+
+		}
+	}
+}
